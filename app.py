a5b782dbb92f8dcfaaa9cd0d314ac45a1045b6b7
Manash Bhaduri <bhaduri.maharshi@gmail.com>
2021-02-16 17:29:09 +0530

    Modifications

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
